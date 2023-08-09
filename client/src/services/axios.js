a39f181d8326cc168899223faab871798f91112e
Anabil <91933794+Anabil-Baruah@users.noreply.github.com>
2023-08-10 00:23:47 +0530

    Added production base url

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
+// const baseURL = 'http://localhost:8080';
+const baseURL = 'https://cryptomedia.onrender.com';
 
 const jwtToken = localStorage.getItem('accessToken');
 axios.defaults.headers.common['Authorization'] = `${jwtToken}`;
 
 export default axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: baseURL,
 })
